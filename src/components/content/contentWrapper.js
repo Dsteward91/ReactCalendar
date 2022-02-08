65ac535d3f5d8c62823ade3e377070d868ddafa7
DavidSteward <steward.david.h@gmail.com>
2022-02-07 20:49:50 -0500

    added live month update functionality

--- src/components/content/contentWrapper.js
+++ src/components/content/contentWrapper.js
@@ -1,9 +1,57 @@
 import React from "react"
 
+import DayOfWeek from "./dayOfWeek"
+import CalendarBox from "./calendarBox"
+
 export default function contentWrapper(props) {
+    const renderPreviousCalendarBoxes = () => {
+        const previousCalendarBoxes = []
+
+        for (let i=1; i<=props.startDay; i++) {
+            previousCalendarBoxes.push(
+                <CalendarBox date={props.daysInPreviousMonth - (props.startDay - i)} />
+            )
+        }
+
+        return previousCalendarBoxes
+    }
+
+    const renderCalendarBoxes = () => {
+        const calendarBoxes = []
+
+        for (let i=1; i<=props.daysInMonth; i++) {
+            calendarBoxes.push(
+                <CalendarBox date={i} />
+            )
+        }
+
+        return calendarBoxes
+    }
+
+    const renderNextCalendarBoxes = () => {
+        const nextCalendarBoxes = []
+
+        for (let i=1; i<=(42 - props.daysInMonth - props.startDay); i++) {
+            nextCalendarBoxes.push(
+                <CalendarBox date={i} />
+            )
+        }
+
+        return nextCalendarBoxes
+    }
+
     return (
         <div className="content-wrapper">
-            Content goes here...
+            <DayOfWeek day="Sunday" />
+            <DayOfWeek day="Monday" />
+            <DayOfWeek day="Tuesday" />
+            <DayOfWeek day="Wednesday" />
+            <DayOfWeek day="Thursday" />
+            <DayOfWeek day="Friday" />
+            <DayOfWeek day="Saturday" />
+            {renderPreviousCalendarBoxes()}
+            {renderCalendarBoxes()}
+            {renderNextCalendarBoxes()}
         </div>
     )
 }
